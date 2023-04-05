0cabe448e57a03b748eebcd49c59f8b2946beebf
mariamacias10 <129053424+mariamacias10@users.noreply.github.com>
2023-04-05 13:21:51 +0200

    Update streamlit_app.py
    reorder

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,20 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(fruits_to_show)
 
 #Lesson 9 (Badge 2 SnowFlake)
-#Display fruityvice api response
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
-      streamlit.write('The user entered ', fruit_choice)
+      back_from_function = get_fruityvice_data(fruity_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
