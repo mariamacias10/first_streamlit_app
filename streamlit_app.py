dfbd1c5166e5fafea2fed63db345418bbae5792b
mariamacias10 <129053424+mariamacias10@users.noreply.github.com>
2023-04-05 13:03:13 +0200

    Update streamlit_app.py
    try exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,20 @@ streamlit.dataframe(fruits_to_show)
 #Lesson 9 (Badge 2 SnowFlake)
 #Display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
+      streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+  streamlit.error()
 
 #Lesson 12
 
